e127c6358b9ebd951fff8182878b749c7648aa4d
Tiago Felipe <tiagosantos0412@gmail.com>
2022-10-27 13:55:01 -0300

    Exercicios de importação otimizados

--- Importacao/main.js
+++ Importacao/main.js
@@ -2,23 +2,14 @@
 
 const { gets, print } = require('./funcoes.auxiliares');
 
-const contador = gets();
+const lista = gets();
+let maiorValorEncontrado = 0;
 
-const numerosSorteados = [];
-
-for(let i = 0; i < contador; i++){
-    const numeroSorteados = gets();
-    numerosSorteados.push(numeroSorteados);
-}
-
-print(numerosSorteados);
-
-let maiorValor = 0;
-
-for(let i=0; i < numerosSorteados.length; i++){
-    const numeroSorteados = numerosSorteados[i];
-    if(numeroSorteados > maiorValor){
-        maiorValor = numeroSorteados;
+for(let i  =0; i < lista; i++){
+    const numeroSorteado = gets();
+    if(numeroSorteado > maiorValorEncontrado){
+        maiorValorEncontrado = numeroSorteado;
     }
 }
-print(maiorValor);
+
+print(maiorValorEncontrado);
